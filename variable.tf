bcdcc68e737b848bb112b46b6f533247c26b24f1
Saravanang12 <saravanang7418@gmail.com>
2025-03-29 10:29:54 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,10 +1,4 @@
-variable subscription {
-    type = string
-    default = "428a821c-ad79-4bec-afb0-45b39b2893c5"
-}
-
-variable tenant {
-    type = string
-    default = "b4e7b79a-4313-4444-aff4-9e7c1c6483d4"
-}
-
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
