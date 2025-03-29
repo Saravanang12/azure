5517121944bb8cfc8fcf85a6b4fbe66071a87364
Saravanang12 <saravanang7418@gmail.com>
2025-03-29 19:33:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
-
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.25.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "Terraform-rg"
+    storage_account_name = "saro97"
+    container_name       = "tfstate"
+    key                 = "terraform.tfstate"
   }
 }
 
