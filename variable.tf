a9c054737a90c7dc559877d86bd96624cd22e10c
Saravanang12 <saravanang7418@gmail.com>
2025-03-31 12:05:27 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,1 @@
-variable "client_id" {
-  description = "Azure Service Principal Client ID"
-  type        = string
-}
 
-variable "client_secret" {
-  description = "Azure Service Principal Client Secret"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
